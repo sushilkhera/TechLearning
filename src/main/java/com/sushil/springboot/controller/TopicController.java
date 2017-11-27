a8caeb469e2280d6c4844e2f092c32b8b6b377c2
Sushil Khera <sushilkhera@gmail.com>
2017-11-26 22:03:09 -0600

    Added comments

--- src/main/java/com/sushil/springboot/controller/TopicController.java
+++ src/main/java/com/sushil/springboot/controller/TopicController.java
@@ -12,7 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sushil.springboot.beans.Topic;
 import com.sushil.springboot.service.TopicService;
-
+/**
+ * 
+ * @author sushilkhera
+ *
+ */
 @RestController
 @RequestMapping("topics")
 public class TopicController {
@@ -20,11 +24,15 @@ public class TopicController {
 	@Qualifier("topicImpl")
 	TopicService topicService;
 	
+	//http://localhost:8081/TechLearningApp/topics/1
+	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public Topic getTopic(@PathVariable("id") long topicId) {
 		printMessage("Rest Controller Topic id::"+topicId);
 		return topicService.getTopic(topicId);
 	}
+	
+	//http://localhost:8081/TechLearningApp/topics/add
 	
 	@RequestMapping(path="/add",method=RequestMethod.POST)
 	public void addTopic(@RequestBody Topic topic) {
@@ -33,11 +41,15 @@ public class TopicController {
 		topicService.addTopic(topic);
 	}
 	
+	//http://localhost:8081/TechLearningApp/topics/all
+	
 	@RequestMapping(path="/all",method=RequestMethod.GET)
 	public List<Topic> getAllTopics(){
 		printMessage("Inside getAllTopics()");
 		return topicService.getAllTopics();
 	}
+	
+	//http://localhost:8081/TechLearningApp/topics/update/1
 	
 	@RequestMapping(path="/update/{id}",method=RequestMethod.PUT)
 	public void updateTopic(@PathVariable("id") long topicId,@RequestBody Topic topic) {
@@ -46,6 +58,8 @@ public class TopicController {
 		printMessage("Received topic for update::"+topic.toString());
 		topicService.updateTopic(topic);
 	}
+	
+	//http://localhost:8081/TechLearningApp/topics/delete/1
 	
 	@RequestMapping(path="/delete/{id}",method=RequestMethod.DELETE)
 	public void deleteTopic(@PathVariable("id") long topicId) {
