8b7ef2f601e5bf09f839afcab25ab4a91790b07e
Sushil Khera <sushilkhera@gmail.com>
2017-11-26 22:08:54 -0600

    Added Comments

--- src/main/java/com/sushil/springboot/TechLearningApplication.java
+++ src/main/java/com/sushil/springboot/TechLearningApplication.java
@@ -2,8 +2,18 @@ package com.sushil.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages= {"com.sushil.springboot","com.sushil.springboot.controller"})
+/**
+ * 
+ * @author sushilkhera
+ *
+ */
+@SpringBootApplication(scanBasePackages= {"com.sushil.springboot",
+		"com.sushil.springboot.controller",
+		"com.sushil.springboot.config",
+		"com.sushil.springboot.service",
+		"com.sushil.springboot.service.impl",
+		"com.sushil.springboot.repo"
+})
 public class TechLearningApplication {
 
 	public static void main(String[] args) {
