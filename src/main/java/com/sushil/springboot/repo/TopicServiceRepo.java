c01c2f02fbb68138b11551a8c3d3b1528d2dd5ef
Sushil Khera <sushilkhera@gmail.com>
2017-11-26 22:08:29 -0600

    Added Comments

--- src/main/java/com/sushil/springboot/repo/TopicServiceRepo.java
+++ src/main/java/com/sushil/springboot/repo/TopicServiceRepo.java
@@ -3,7 +3,11 @@ package com.sushil.springboot.repo;
 import org.springframework.data.repository.CrudRepository;
 
 import com.sushil.springboot.beans.Topic;
-
+/**
+ * 
+ * @author sushilkhera
+ *
+ */
 public interface TopicServiceRepo extends CrudRepository<Topic, Long>{
 	
 	public Topic findByTopicId(long topicID);
